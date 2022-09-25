d623321ff77663fbe2851f16b1a7bf228ac71093
MRomano2 <103249614+MRomano2@users.noreply.github.com>
2022-09-25 18:20:41 +0200

    Update WebGLNodes.js

--- jsm/renderers/webgl/nodes/WebGLNodes.js
+++ jsm/renderers/webgl/nodes/WebGLNodes.js
@@ -1,7 +1,7 @@
 import { WebGLNodeBuilder } from './WebGLNodeBuilder.js';
-import { NodeFrame } from 'three/nodes';
+import { NodeFrame } from  "../../../jsm/nodes/Nodes.js";
 
-import { Material } from 'three';
+import { Material } from '../../../../build/three.module.js';
 
 const builders = new WeakMap();
 export const nodeFrame = new NodeFrame();
