9a0632167e0a609f9a8f35be777e772f16cb4917
MRomano2 <103249614+MRomano2@users.noreply.github.com>
2022-09-25 18:38:29 +0200

    Update FBXLoader.js

--- FBXLoader.js
+++ FBXLoader.js
@@ -40,9 +40,9 @@ import {
 	Vector4,
 	VectorKeyframeTrack,
 	sRGBEncoding
-} from 'three';
-import * as fflate from '../libs/fflate.module.js';
-import { NURBSCurve } from '../curves/NURBSCurve.js';
+} from './build/three.module.js';
+import * as fflate from './fflate.module.js';
+import { NURBSCurve } from './NURBSCurve.js';
 
 /**
  * Loader loads FBX file and generates Group representing FBX scene.
