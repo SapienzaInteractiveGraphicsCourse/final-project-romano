ded20a35c44cd8545ced9de56e22af3dbb91b514
MRomano2 <103249614+MRomano2@users.noreply.github.com>
2022-09-25 19:48:42 +0200

    Update NodeMaterial.js

--- jsm/nodes/materials/NodeMaterial.js
+++ jsm/nodes/materials/NodeMaterial.js
@@ -1,4 +1,4 @@
-import { Material, ShaderMaterial } from 'three';
+import { Material, ShaderMaterial } from '../../../build/three.module.js';
 import { getNodesKeys, getCacheKey } from '../core/NodeUtils.js';
 import ExpressionNode from '../core/ExpressionNode.js';
 import {
